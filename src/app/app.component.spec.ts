95fba5e3484c2dd9bf97ecbd2ce04849470a5e0c
Abhilash Asokan <abhilash.asokan@logicline.de>
2019-10-19 12:49:51 +0530

    mock component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
-@Component({
-  selector: 'app-header',
-  template: ''
-})
-export class HeaderStubComponent {}
+// @Component({
+//   selector: 'app-header',
+//   template: ''
+// })
+// export class HeaderStubComponent {}
 
-@Component({
-  selector: 'app-footer',
-  template: ''
-})
+// @Component({
+//   selector: 'app-footer',
+//   template: ''
+// })
 export class FooterStubComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent, HeaderStubComponent, FooterStubComponent],
+      declarations: [
+        AppComponent,
+        MockComponent(HeaderComponent),
+        MockComponent(FooterComponent)
+      ],
       imports: [RouterTestingModule]
     }).compileComponents();
   }));
